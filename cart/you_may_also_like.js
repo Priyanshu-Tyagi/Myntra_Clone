083a55fb2b044baefcf7f1b5b0714d787d906484
Priyanshu-Tyagi <102712899+Priyanshu-Tyagi@users.noreply.github.com>
2022-05-06 21:09:11 +0530

    60% completed

--- cart/you_may_also_like.js
+++ cart/you_may_also_like.js
@@ -1,65 +1,4 @@
-//            SHOWMORE AND SHOWLESS
-
-function showmore() {
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("showmore");
-  
-     if(moreText.style.display === "block"){
-        moreText.style.display = "none"
-    }
-    else {
-      btnText.innerHTML = "Show less"; 
-      moreText.style.display = "block";
-    }
-  }
-
-  document.querySelector("#filterdiv>div").addEventListener("click",function(){
-    document.querySelector("#filterdiv>div").style.border="1px solid #ff3f6c";
-    document.querySelector("#filterdiv>div").style.color="#ff3f6c"
-  })
-
-  //            YOU MAY ALSO LIKE:
-
-  window.addEventListener("load",function(){displayyoumayalsolike(Data)});
-
-  function displayyoumayalsolike(Data){
-    Data.map(function(el){
-      var card=document.createElement("div");
-      card.id="card";
-
-      var image=document.createElement("img");
-      image.setAttribute("src",el.image);
-
-      var name=document.createElement("div");
-      name.innerText=el.name;
-
-      var desc=document.createElement("div");
-      desc.innerText=el.desc;
-      desc.id="youlikedesc";
-
-      var prices=document.createElement("div");
-      prices.id="pricesdiv";
-      var price=document.createElement("p");
-      price.innerText=el.price;
-      price.id="youlikeprice";
-      var pricecut=document.createElement("p");
-      pricecut.innerText=el.pricecut;
-      pricecut.id="youlikepricecut";
-      var off=document.createElement("p");
-      off.innerText=el.off;
-      off.id="youlikeoff";
-      prices.append(price,pricecut,off)
-
-      var addtocart=document.createElement("div");
-      addtocart.innerText="ADD TO CART";
-      addtocart.id="addtocart";
-
-      card.append(image,name,desc,prices,addtocart);
-      document.getElementById("cardcontainer").append(card);
-    })
-  }
-
-  var Data=[
+var Data=[
     {
         image:"https://assets.myntassets.com/q_95,w_172,c_limit,f_webp,fl_progressive/assets/images/17066222/2022/2/7/d3689563-7fb4-47e7-92c3-c450fa5af4d71644212682390ELLISMenBlueSolid1.jpg",
         name:"ELLIS",
@@ -71,7 +10,7 @@ function showmore() {
     {
         image:"https://assets.myntassets.com/q_95,w_172,c_limit,f_webp,fl_progressive/assets/images/16922798/2022/1/25/8da04c85-cca4-4fb7-90c6-00b33424e3901643049088352CalvadossPremiumDesignWovenbroadTie1.jpg",
         name:"Calvadoss",
-        desc:"Floral Woven Design Broad...",
+        desc:"Floral Woven Design Broad Tie",
         price:"₹319",
         pricecut:"₹999",
         off:"(68% OFF)",
@@ -95,7 +34,7 @@ function showmore() {
     {
         image:"https://assets.myntassets.com/q_95,w_172,c_limit,f_webp,fl_progressive/assets/images/productimage/2021/4/8/e311beaa-900f-4362-8790-6b2723dfbe2c1617890175348-2.jpg",
         name:"Alvaro Castagnino",
-        desc:"Men Pack of 3 Patterned S...",
+        desc:"Men Pack of 3 Patterned Socks",
         price:"₹314",
         pricecut:"₹699",
         off:"(55% OFF)",
@@ -103,7 +42,7 @@ function showmore() {
     {
         image:"https://assets.myntassets.com/q_95,w_172,c_limit,f_webp,fl_progressive/assets/images/15144930/2021/8/13/84735b0c-68e8-4423-bb67-3414d0901d701628874255040AlvaroCastagninoBlackGreyBlueColouredSockscomboforMen1.jpg",
         name:"Alvaro Castagnino",
-        desc:"Men Pack Of 3 Assorted A...",
+        desc:"Men Pack Of 3 Assorted Above Ankle-Length Socks",
         price:"₹274",
         pricecut:"₹499",
         off:"(45% OFF)",
